7b2dfb8c664a091909ef5bf8355720c3529e78b2
ha615 <sdet615@gmail.com>
2021-01-26 19:20:57 +0700

    System.out.println("Hello world");

---
+++ src/selenium/Topic_02_Xpath_Css.java
@@ -0,0 +1,32 @@
+package selenium;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class Topic_02_Xpath_Css {
+
+	WebDriver driver;
+	@BeforeClass
+	public void beforeClass() {
+		driver = new FirefoxDriver();
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		driver.manage().window().maximize();
+		driver.get("https://alada.vn/tai-khoan/dang-ky.html");
+	}
+	@Test
+	public void loginForm() {
+		driver.findElement(By.id("txtFirstname")).sendKeys("Luong Khong Nho");
+		driver.findElement(By.id("txtEmail")).sendKeys("lkn@gmail.com");
+		driver.findElement(By.id("txtCEmail")).sendKeys("lkn@gmail.com");
+	}
+	@AfterClass
+	public void afterClass() {
+		driver.quit();
+	}
+}
--- src/selenium/Topic_02_xpath_css.java
+++
@@ -1,7 +0,0 @@
-package selenium;
-
-public class Topic_02_xpath_css {
-	public void SomeThing() {
-		System.out.println("Hello world");
-	}
-}
